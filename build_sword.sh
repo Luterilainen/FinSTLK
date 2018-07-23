99725872b1a58af21be1bb981f32698156ff0ced
Tuomas Airaksinen <tuomas.airaksinen@gmail.com>
2018-07-23 21:43:57 +0300

    Viimeisiä muutoksia Ilpon pyynnöstä

--- build_sword.sh
+++ build_sword.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-mkdir -p stlk_mod/modules/texts/ztext/STLK2017/ 
+mkdir -p stlk_mod/modules/texts/ztext/FinSTLK/ 
 mkdir -p stlk_mod/mods.d/
-cp STLK2017.conf stlk_mod/mods.d/
-osis2mod stlk_mod/modules/texts/ztext/STLK2017/ stlk2017.xml -v German -z
+cp FinSTLK.conf stlk_mod/mods.d/
+osis2mod stlk_mod/modules/texts/ztext/FinSTLK/ FinSTLK.xml -v German -z
 cd stlk_mod
 rm *.zip
-zip stlk2017.zip -r mods.d/*.conf modules
-cp stlk2017.zip ..
+zip FinSTLK.zip -r mods.d/*.conf modules
+cp FinSTLK.zip ..
 #unzip -d ~/.sword/ stlk2017.zip
 cd ..
 rm -r stlk_mod
